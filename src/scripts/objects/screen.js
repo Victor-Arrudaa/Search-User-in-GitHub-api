334b8cabb7c1a11df320d0ef270c0083447bdc54
Victor Arruda <victor0070000@gmail.com>
2023-08-03 20:47:28 -0300

    displaying user events on screen

--- src/scripts/objects/screen.js
+++ src/scripts/objects/screen.js
@@ -1,14 +1,16 @@
 const screen = {
     userProfile: document.querySelector(".profile-data"),
     renderUser(user) {
-     this.userProfile.innerHTML = `
+        this.userProfile.innerHTML = `
             <div class=info>
                 <img src= "${user.avatarUrl}" alt="Foto do perfil do usuário">
                 <div class="data">
                     <h1>${user.name ?? "Não possui nome cadastrado 😢"}</h1>
                     </br>
                     <p>${user.bio ?? "Não possui bio cadastrada 😢"}</p>
-                    <p style="display: inline">Seguidores: ${user.followers}</p>
+                    <p style="display: inline">Seguidores: ${
+                        user.followers
+                    } |</p>
                     <p style="display: inline">Seguindo: ${user.following}</p>
                 </div>
             </div>`;
@@ -24,6 +26,23 @@ const screen = {
                 <h2>Repositórios</h2>
                 <ul>
                     <li>${repositoriesItens}</li>
+                </ul>
+            </div>`;
+        }
+
+        let eventItens = "";
+        user.events.forEach(
+            (event) =>
+                (eventItens += `<li><h3 style="display: inline">${event.repo.name} - </h3><p>${event.payload.commits[0].message}</p></li>`)
+        );
+
+        console.log(user.events);
+
+        if (user.events.length > 0) {
+            this.userProfile.innerHTML += `<div class = events section>
+                <h2>Eventos</h2>
+                <ul>
+                    <li>${eventItens}</li>
                 </ul>
             </div>`;
         }
